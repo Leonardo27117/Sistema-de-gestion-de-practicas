fdabf982deb8cb486561a3cb22cf1cecf014e093
Leonardo27117 <zS23014080@estudiantes.uv.mx>
2025-04-29 15:04:29 -0600

    primer commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.sistemagestionpracticas {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens gui to javafx.fxml;
+}
